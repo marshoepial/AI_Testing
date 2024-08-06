68127a864dae59e7591947f611c5f45c14d74f1e
Abiodun Ganiyu <abiodunganiyu@Abioduns-MacBook-Pro.local>
2024-08-06 16:58:13 -0400

    removed vercel database

--- app/seed/routes.ts
+++ app/seed/routes.ts
@@ -1,41 +1,41 @@
-import { db } from '@vercel/postgres';
+// import { db } from '@vercel/postgres';
 
 
-const client = await db.connect();
+// const client = await db.connect();
 
-async function seedFunctionalTestCases() {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    await client.sql`
-        CREATE TABLE IF NOT EXISTS functional (
-            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL
-            testDetail JSONB,
-            setup VARCHAR(255),
-            procedure VARCHAR(255) NOT NULL,
-        )`
-}
+// async function seedFunctionalTestCases() {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//     await client.sql`
+//         CREATE TABLE IF NOT EXISTS functional (
+//             id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//             title VARCHAR(255) NOT NULL,
+//             description VARCHAR(255) NOT NULL
+//             testDetail JSONB,
+//             setup VARCHAR(255),
+//             procedure VARCHAR(255) NOT NULL,
+//         )`
+// }
 
-export async function GET() {
-    try {
-        await client.sql`BEGIN`;
-        await seedFunctionalTestCases();
-        await client.sql`COMMIT`;
+// export async function GET() {
+//     try {
+//         await client.sql`BEGIN`;
+//         await seedFunctionalTestCases();
+//         await client.sql`COMMIT`;
 
-        return Response.json({ message: 'Database seeded successfully'});
-    } catch (error) {
-        await client.sql`ROLLBACK`;
-        return Response.json({ error }, { status: 500 });
-    }
-}
+//         return Response.json({ message: 'Database seeded successfully'});
+//     } catch (error) {
+//         await client.sql`ROLLBACK`;
+//         return Response.json({ error }, { status: 500 });
+//     }
+// }
 
-async function seedPerformanceTestCase() {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    await client.sql`
-        CREATE TABLE IF NOT EXISTS performance (
-            id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            
-        )
-    `
-}
+// async function seedPerformanceTestCase() {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//     await client.sql`
+//         CREATE TABLE IF NOT EXISTS performance (
+//             id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//             title VARCHAR(255) NOT NULL,
+
+//         )
+//     `
+// }
